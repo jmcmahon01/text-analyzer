e92c96541af552947c18b22bfbdaede64c5b9f05
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-11-09 08:59:29 -0800

    add UI logic and fix bug in numberOfOccurences function if input is empty string

--- README.md
+++ README.md
@@ -69,4 +69,9 @@ const word = "Red";
 numberOfOccurrencesInText(word, text);
 Expected Output: 3
 
-Describe: removeElements
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+numberOfOccurrencesInText(word, text);
+Expected Output: 0
--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@ function wordCounter(text) {
   }
   let wordCount = 0;
   const textArray = text.split(" ");
-  textArray.forEach(function(element) {
+  textArray.forEach(function (element) {
     if (!Number(element)) {
-      wordCount++; 
+      wordCount++;
     }
   });
   return wordCount;
@@ -18,9 +18,12 @@ function wordCounter(text) {
 //trim() => "   HELLO!   " => "HELLO!" 
 
 function numberOfOccurrencesInText(word, text) {
+  if (word.trim().length === 0) {
+    return 0;
+  }
   const textArray = text.split(" ");
   let wordCount = 0;
-  textArray.forEach(function(element) {
+  textArray.forEach(function (element) {
     if (element.toLowerCase().includes(word.toLowerCase())) {
       wordCount++;
     }
@@ -28,6 +31,23 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
-//UI Logic
 
+
+
+
+// UI Logic
+
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function () {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
 
